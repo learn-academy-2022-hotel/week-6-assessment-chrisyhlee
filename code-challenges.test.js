2e96634e87aa2dbbc261e401d16e21e1702e5c06
chrisyhlee <leechr8393@gmail.com>
2023-01-29 14:14:48 -0800

    completed assessment for week 6

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,9 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+// const { number } = require("yargs")
+
+// const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -21,7 +25,43 @@ const people = [
 ]
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+describe ('capitalizeMe', () => {
+  it ('takes in an array of objects and returns an array with a sentence about each person with their name capitalized', () => {
+    expect (capitalizeMe(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// capitalizeMe
+//   ✕ takes in an array of objects and returns an array with a sentence about each person with their name capitalized (1 ms)
+
+// ● capitalizeMe › takes in an array of objects and returns an array with a sentence about each person with their name capitalized
+
+//   ReferenceError: capitalizeMe is not defined
+
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE
+// create fxn called capitalizeMe
+// input: array of objects
+// output: array with a sentence about each person with their name capitalized
+// iterate through array to access each object
+// use dot notation to access the key 'name' in each object
+// iterate through string and .toUpperCase index 0 and the letter after any space
+
+const capitalizeMe = (arr) => {
+  return arr.map((value) => {
+    let splitArr = value.name.split(' ')
+    let emptyArr = []
+    splitArr.map((value, i) => {
+      return emptyArr.push(value[0].toUpperCase() + value.substr(1) + ' ')
+    })
+    return `${emptyArr.join('')}is ${value.occupation}.`
+  });
+}
+// PASS  ./code-challenges.test.js
+// capitalizeMe
+//   ✓ takes in an array of objects and returns an array with a sentence about each person with their name capitalized (1 ms)
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
@@ -32,7 +72,40 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe ('remainders', () => {
+  it ('takes in a mixed data array and returns an array of only the remainders of the numbers when divided by 3', () => {
+    expect (remainders(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect (remainders(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// remainders
+//   ✕ takes in a mixed data array and returns an array of only the remainders of the numbers when divided by 3
+
+// ● remainders › takes in a mixed data array and returns an array of only the remainders of the numbers when divided by 3
+
+//   ReferenceError: remainders is not defined
+
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE
+// create function remainders
+// input: array of mixed data types
+// output: array of remainders when divided by 3
+// use filter to get array of only numbers
+// iterate through new array of only numbers for remainder when divided by three
+
+const remainders = (arr) => {
+  let numArr =  arr.filter(value => typeof value === 'number')
+  return numArr.map((value) => {
+    return value % 3
+  })
+}
+
+// PASS  ./code-challenges.test.js
+// remainders
+//   ✓ takes in a mixed data array and returns an array of only the remainders of the numbers when divided by 3
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -43,4 +116,35 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe ('sumCubed', () => {
+  it ('takes in an array of numbers and returns the sum of all the numbers cubed', () => {
+    expect (sumCubed(cubeAndSum1)).toEqual(99)
+    expect (sumCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// sumCubed
+//   ✕ takes in an array of numbers and returns the sum of all the numbers cubed
+
+// ● sumCubed › takes in an array of numbers and returns the sum of all the numbers cubed
+
+//   ReferenceError: sumCubed is not defined
+
 // b) Create the function that makes the test pass.
+
+// PSUEDOCODE
+// create function sumCubed
+// input: array of numbers
+// output: sum of each number cubed
+// iterate through array and cube each value
+// add all the new numbers together
+
+const sumCubed = (arr) => {
+  cubedArr = arr.map(value => value*value*value)
+  return cubedArr.reduce((a,b) => a + b, 0)
+}
+
+// PASS  ./code-challenges.test.js
+// sumCubed
+//   ✓ takes in an array of numbers and returns the sum of all the numbers cubed (1 ms)
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) This is an example of inheritance. The class BlogPostsController is inheriting from the class ApplicationController. This means that BlogPostsController will be able to access attributes and behaviors within ApplicationController.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) @posts is an instance variable being assigned to BlogPost.all, which will display all the values contained within the model BlogPost.
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) Method for the controller action 'show', which displays the details of one specific item. In this case, the method is displaying the details of a column in the model BlogPost, which is specified using the symbol :id, which corresponds to the primary key of the column. This corresponds with the http verb 'get' and the CRUD action of 'read'.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) Method for the controller action 'new', which will display a form for the user to fill out in order to create something new. This corresponds to the http verb 'get' and the CRUD action of 'read'.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) Method for the controller action 'create', which takes the data that was filled out with 'new' and actually updates the database with that data. This corresponds to the http verb 'post' and the CRUD action of 'create'.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -32,13 +32,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) Method for the controller action 'edit', which displays a form for the user to fill out in order to update data that already exists in the database. This corresponds with the http verb 'get' and the CRUD action of 'read'.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) @post.update is using the method of update on the instance variable @post in order to update the database based on the form filled out from the 'edit' controller action. It is passed blog_post_params, which is a strong params that gives permission to the user to only modify data that is specified within the strong params method.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -48,15 +48,15 @@ class BlogPostsController < ApplicationController
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
-      # ---8)
+      # ---8) Used to redirect the user to the path indicated. In this case, if the user deletes data from the database, the user is then redirected to the route with the alias of 'blog_posts'.
       redirect_to blog_posts_path
     end
   end
 
-  # ---9)
+  # ---9) Private is used to contain anything below its line to the scope of the current class. In this example, the strong param blog_post_params can only be called on and used within the class of BlogPostsController.
   private
   def blog_post_params
-    # ---10)
+    # ---10) This is a strong param, which provides protections for the database by specifying required attributes that must be filled out in order for the database to be updated. This example is requiring the data of title and content from the model BlogPost.
     params.require(:blog_post).permit(:title, :content)
   end
 end
